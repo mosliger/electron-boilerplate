a81cda6102c38960b1facfbe321d92a87d98d28a
mosliger <chonlatee200436@gmail.com>
2019-10-25 12:31:53 +0700

    create file

--- public/electron.js
+++ public/electron.js
@@ -1,20 +1,54 @@
 const electron = require('electron');
+const log = require('electron-log');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
+const fs = require('fs');
 const isDev = require('electron-is-dev');
-
+const ipcMain = electron.ipcMain;
 let mainWindow;
 
+function createFile(value) {
+  fs.readFile('users.json', 'utf8', function readFileCallback(err, data){
+    log.info(value);
+    if (err){
+      log.error(err);
+      fs.writeFile('users.json', JSON.stringify([value]), 'utf8', function callback() {});
+    } else {
+      let users = [];
+      if (data.length > 0) users = JSON.parse(data); //now it an object
+      users.push(value)
+      fs.writeFile('users.json', JSON.stringify(users), 'utf8', function callback() {}); // write it back 
+    }
+  });
+}
+
+ipcMain.on('create-user',  (event, value) => {
+  createFile(value)
+});
+
+ipcMain.on('get-user',  (event) => {
+  fs.readFile('users.json', 'utf8', function readFileCallback(err, data){
+    event.reply('user-reply', data)
+  })
+});
+
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  if (isDev) {
+  // if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
     mainWindow.webContents.openDevTools();
-  }
+  // }
+
   mainWindow.on('closed', () => mainWindow = null);
 }
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [valueInput, setValueInput] = useState('')
+
+  const { ipcRenderer } = window.require('electron');
+
+  ipcRenderer.on('user-reply', (event, arg) => {
+    console.log('arg >>>', arg);
+   if (arg !== null) setUsers(JSON.parse(arg))
+  })
+
+  const handleCreateUser = () => {
+    try {
+      ipcRenderer.send('create-user', { username: valueInput });
+      setValueInput('')
+    } catch (err) {
+      console.log('err', err);
+    }
+  }
+
+  const getUser = () => {
+    try {
+      ipcRenderer.send('get-user');
+    } catch (err) {
+      console.log('err', err);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +37,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <input type="text" value={valueInput} onChange={e => setValueInput(e.target.value)}/>
+          <button onClick={handleCreateUser}>Create User</button>
+        </p>
+        <button onClick={getUser}>get User</button>
+        <h2>User</h2>
+        <ul>
+          {users.map((value) => (<li key={value.username}>{value.username}</li>))}
+        </ul>
       </header>
     </div>
   );
